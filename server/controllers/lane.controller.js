bc138d2999c12034b9bcf5b62531e11aae356991
Jaroslaw Mejer <jaroslaw.mejer@gmail.com>
2018-08-23 18:46:50 +0200

    Add few endpoints

---
+++ server/controllers/lane.controller.js
@@ -0,0 +1,52 @@
+import uuid from 'uuid';
+import Lane from '../models/lane';
+
+
+export function addLane(req, res) {
+  if (!req.body.name) {
+    res.status(403).end();
+  }
+
+  const newLane = new Lane(req.body);
+
+  newLane.notes = [];
+
+  newLane.id = uuid();
+  newLane.save((err, saved) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json(saved);
+  });
+}
+
+export function getLanes(req, res) {
+  Lane.find().exec((err, lanes) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json({ lanes });
+  });
+}
+
+export function deleteLane(req, res) {
+  Lane.findOne({ id: req.params.laneId }).exec((err, lane) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+
+    lane.remove(() => {
+      res.status(200).end();
+    });
+  });
+}
+
+export function editLaneName(req, res) {
+	Lane.update({id: req.params.laneId}, req.body).exec((err, lane) =>{
+		if (err) {
+			res.status(500).send(err);
+		}
+
+		res.json({ lane })
+	})
+}
